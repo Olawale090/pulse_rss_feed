287d1a17b18ebb1397c3cfe3dbad0c09e7a05d35
Israel Olawale <mosesolawale21@gmail.com>
2022-04-18 19:11:38 +0100

    removing unused dependencies

--- app.js
+++ app.js
@@ -9,26 +9,16 @@ app.use("/carrier",express.static('carrier'))
 app.use("/style",express.static('style'))
 app.use("/assets",express.static('assets'))
 
-app.get("/query_string/",(req,res)=>{
-
-    parse('https://www.pulse.com.gh/news/rss').then(rss => {
-
-        res.end(JSON.stringify(rss, null, 3));
-
-    });
-
-})
-
-
-
 app.get("/page",(req,res)=>{
 
     Feed.convert('https://www.pulse.com.gh/news/rss')
         .then(function(json) {
 
             var data_pipe = json;
+            
             // console.log(data_pipe.items[0].enclosures); this block of data is not accessible!!!
-            // console.log(data_pipe.items[0].content_encoded); this block of data is no accessible!!!
+            // console.log(data_pipe.items[0].content_encoded); this block of data is not accessible!!!
+
             return res.render('page',{ info:data_pipe });
         })
     .catch(function(err) {
